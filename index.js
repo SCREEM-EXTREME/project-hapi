bfb7d0e4ad94b61b2af5da01ab64b281d91c6ecd
Kenneth chan <werg2c@yahoo.com.hk>
2015-04-13 06:39:28 +0000

    hapi updates

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-
 var Hapi = require('hapi');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
-    port: 8001 
+    host: '0.0.0.0', 
+    port: 8080,
+    routes: {cors: true}
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello', 
-    handler: function (request, reply) {
-       reply('hello world');
-    }
+var plugins = [{ register: require('./routes/quotes.js') }];
+
+server.register(plugins, function (err) {
+  if (err) { throw err; }
+
+  server.start(function () {
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
 });
 
-// Start the server
 server.start();
